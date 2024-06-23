96724df4a575a5cf46d12fcb28377f59d2645838
hiroki <hir_koba.1389@outlook.com>
2024-06-23 19:02:01 +0900

    機能追加 6/23
    ・若干のUI修正
    ⇨ スタートボタンの大きさ調整
    ⇨ ボタンテキストの大文字化
    ⇨ RestartボタンをRESUMEボタンに変更
    ・ゲーム終了時の負け表示
    ・ゲーム終了後の本当のリスタートボタン
    ・サブタイマー以下の秒数で警告出す

--- app/src/main/java/com/ihiroki/mahjongsoultimer/MainActivity.kt
+++ app/src/main/java/com/ihiroki/mahjongsoultimer/MainActivity.kt
@@ -7,146 +7,191 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.ceil
+import android.graphics.Color
 
 class MainActivity : AppCompatActivity() {
 
-    private var player1Time = 20.0f
-    private var player2Time = 20.0f
-    private var turnTime = 5.0f
+    // タイマーの時間
+    private var mainTime = 20.0f
+    private var subTime = 5.0f
 
-    private var currentPlayer = 1
-    private var gamePaused = false
+    // タイマーの時間を格納する変数
+    private var player1Time = mainTime
+    private var player2Time = mainTime
+    private var tempTime = subTime
 
+    // 実際に動作するタイマー
+    private lateinit var turnCountDownTimer: CountDownTimer
+    private var playerCountDownTimer: CountDownTimer? = null
+
+    private var currentPlayer = 1 // どっちのプレイヤーのターンか
+    private var gamePaused = false // ゲームが一時停止しているか
+    private var isEnded = false // ゲームが終了しているか
+
+    // ボタンとかテキストとか（これらはonCreateで初期化しないとアプリが落ちる）
     private lateinit var player1TimerView: TextView
     private lateinit var player2TimerView: TextView
     private lateinit var turnButton: Button
     private lateinit var pauseButton: Button
 
-    private lateinit var turnCountDownTimer: CountDownTimer
-    private var playerCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // ボタンとかテキストとかの初期化
         player1TimerView = findViewById(R.id.player1Timer)
         player2TimerView = findViewById(R.id.player2Timer)
         turnButton = findViewById(R.id.turnButton)
         pauseButton = findViewById(R.id.pauseButton)
 
         // 初期表示
-        player1TimerView.text = "Player 1 Timer\n${player1Time.toInt()}"
-        player2TimerView.text = "Player 2 Timer\n${player2Time.toInt()}"
+        player1TimerView.text = "Player 1 Timer\n${player1Time.toInt()} + ${ceil(subTime).toInt()}"
+        player2TimerView.text = "Player 2 Timer\n${player2Time.toInt()} + ${ceil(subTime).toInt()}"
 
+        // タイマーの開始
         startTurnTimer()
 
+        // ターン交代（リスタート）ボタン押された時
         turnButton.setOnClickListener {
-            switchTurn()
+            if (isEnded) restartGame()
+            else switchTurn()
         }
 
+        // 一時停止（再開）ボタン押された時
         pauseButton.setOnClickListener {
-            if (gamePaused) {
-                resumeGame()
-            } else {
-                pauseGame()
+            // ゲーム中のみ反応
+            if (!isEnded) {
+                if (gamePaused) resumeGame()
+                else pauseGame()
             }
         }
     }
 
+    // タイマーの開始
     private fun startTurnTimer() {
-        turnCountDownTimer = object : CountDownTimer((turnTime * 1000).toLong(), 100) {
-            override fun onTick(millisUntilFinished: Long) {
-                if (!gamePaused) {
-                    turnTime = millisUntilFinished / 1000.0f
-                    updateTurnTimerView(turnTime)
-                }
-            }
+        // 一時停止してる時は何もしない
+        if (!gamePaused) {
+            // タイマーの作成
+            turnCountDownTimer = object : CountDownTimer((tempTime * 1000).toLong(), 100) {
+                // countDownInterval毎に呼び出される関数
+                override fun onTick(millisUntilFinished: Long) {
+                    if (tempTime <= 0.1) return // サブタイマーがほぼ終了なら表示更新しない
+                    tempTime = millisUntilFinished / 1000.0f // 表示用変数の更新
 
-            override fun onFinish() {
-                if (!gamePaused) {
-                    turnTime = 0.0f
-                    updateTurnTimerView(turnTime)
-                    startPlayerTimer()
+                    // タイマーの表示を更新する
+                    if (currentPlayer == 1) {
+                        if(player1Time + tempTime < subTime) player1TimerView.setTextColor(Color.RED) // サブタイマー以下なら警告色（赤）にする
+                        player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()} + ${ceil(tempTime).toInt()}"
+                        player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()} + ${ceil(subTime).toInt()}"
+                    } else {
+                        if(player2Time + tempTime < subTime) player2TimerView.setTextColor(Color.RED) // サブタイマー以下なら警告色（赤）にする
+                        player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()} + ${ceil(subTime).toInt()}"
+                        player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()} + ${ceil(tempTime).toInt()}"
+                    }
+                }
+
+                // タイマーが終了したら呼び出される
+                override fun onFinish() {
+                    startPlayerTimer() // プレイヤー固有のタイマー開始
                 }
             }
+            turnCountDownTimer.start()
         }
-        turnCountDownTimer.start()
     }
 
+    // プレイヤー固有のタイマー開始
     private fun startPlayerTimer() {
+        // 一時停止してる時は何もしない
         if (!gamePaused) {
-            val playerTime = if (currentPlayer == 1) player1Time else player2Time
+            val playerTime = if (currentPlayer == 1) player1Time else player2Time // 現在プレイヤーのタイマーを変数を参照する
 
+            // タイマーの作成
             playerCountDownTimer = object : CountDownTimer((playerTime * 1000).toLong(), 100) {
+                // countDownInterval毎に呼び出される関数
                 override fun onTick(millisUntilFinished: Long) {
-                    if (!gamePaused) {
-                        val remainingTime = millisUntilFinished / 1000.0f
-                        if (currentPlayer == 1) {
-                            player1Time = remainingTime
-                            player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()}"
-                        } else {
-                            player2Time = remainingTime
-                            player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()}"
-                        }
+                    val remainingTime = millisUntilFinished / 1000.0f
+                    // タイマーの表示を更新する
+                    if (currentPlayer == 1) {
+                        if(player1Time < subTime) player1TimerView.setTextColor(Color.RED) // サブタイマー以下なら警告色（赤）にする
+                        player1Time = remainingTime // プレイヤー固有のタイマーの更新
+                        player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()}"
+                    } else {
+                        if(player2Time < subTime) player2TimerView.setTextColor(Color.RED) // サブタイマー以下なら警告色（赤）にする
+                        player2Time = remainingTime
+                        player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()}"
                     }
                 }
 
+                // タイマーが終了したら呼び出される
                 override fun onFinish() {
-                    if (!gamePaused) {
-                        if (currentPlayer == 1) {
-                            player1Time = 0.0f
-                            player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()}"
-                            // Player 1 loses
-                        } else {
-                            player2Time = 0.0f
-                            player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()}"
-                            // Player 2 loses
-                        }
-                    }
+                    isEnded = true
+                    if (currentPlayer == 1) player1TimerView.text = "Player 1 Lose"
+                    else player2TimerView.text = "Player 2 Lose"
+
+                    turnButton.text = "RESTART"
+
+                    // タイマーストップ
+                    turnCountDownTimer.cancel()
+                    playerCountDownTimer?.cancel()
                 }
             }
             playerCountDownTimer?.start()
         }
     }
 
+    // ターン交代ボタン押された時
     private fun switchTurn() {
+        // 一時停止してる時は何もしない
         if (!gamePaused) {
+            // 今動いてるタイマーを止める
             turnCountDownTimer.cancel()
             playerCountDownTimer?.cancel()
-            currentPlayer = if (currentPlayer == 1) 2 else 1
-            turnTime = 5.0f
+
+            // プレイヤーの残タイマーを小数点以下切り上げる
             if (currentPlayer == 1) {
-                player2Time = ceil(player2Time).toFloat()
-                player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()}"
-            } else {
-                player1Time = ceil(player1Time).toFloat()
-                player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()}"
+                player1Time = ceil(player1Time)
+                player1TimerView.setTextColor(Color.BLACK) // 黒色に戻す
             }
-            startTurnTimer()
+            else {
+                player2Time = ceil(player2Time)
+                player2TimerView.setTextColor(Color.BLACK) // 黒色に戻す
+            }
+
+            currentPlayer = if (currentPlayer == 1) 2 else 1 // ターン交代
+            tempTime = subTime // サブタイマーを初期化
+            startTurnTimer() // ターン開始
         }
     }
 
-    private fun updateTurnTimerView(time: Float) {
-        if (currentPlayer == 1) {
-            player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()} + ${ceil(time).toInt()}"
-            player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()} + 5"
-        } else {
-            player1TimerView.text = "Player 1 Timer\n${ceil(player1Time).toInt()} + 5"
-            player2TimerView.text = "Player 2 Timer\n${ceil(player2Time).toInt()} + ${ceil(time).toInt()}"
-        }
+    // リスタートボタンが押された時
+    private fun restartGame() {
+        // 変数の初期化
+        player1Time = mainTime
+        player2Time = mainTime
+        tempTime = subTime
+        currentPlayer = 1
+        gamePaused = false
+        isEnded = false
+
+        startTurnTimer()
     }
 
+    // 一時停止ボタン押された時
     private fun pauseGame() {
         gamePaused = true
+        pauseButton.text = "RESUME"
+
+        // 今動いてるタイマーを止める
         turnCountDownTimer.cancel()
         playerCountDownTimer?.cancel()
-        pauseButton.text = "Restart"
     }
 
+    // 再開ボタン押された時
     private fun resumeGame() {
         gamePaused = false
-        startTurnTimer()
-        pauseButton.text = "Pause"
+        pauseButton.text = "PAUSE"
+        startTurnTimer() // タイマー開始する（前の変数初期化してないから前から継続される）
     }
 }
